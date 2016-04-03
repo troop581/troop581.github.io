801aa5bbc63dd052e9ef7751b2f80669fdd7b77e
Wilson, Sean M <sean.m.wilson@intel.com>
2016-04-02 18:13:49 -0700

    2016.04.02

--- app/modules/requirements.js
+++ app/modules/requirements.js
@@ -2,12 +2,60 @@
     'use strict';
     var vm = this;
     vm.data = data;
+    vm.meritBadges = {}
+    vm.meritBadgeUrl = 'meritbadge.org/wiki/index.php/Merit_Badges';
 
     vm.meritBadgeList = [];
 
     function getMeritBadges() {
-        return data.getWebpage('meritbadge.org/wiki/index.php/Merit_Badges', 'ol').then(function (webpage) {
-            console.log('got merit badges...hopefully');
+        return data.getWebpage(url, 'ol').then(function (ol) {
+            var list;
+            ol = _.castArray(ol);
+            _.forEach(ol, function (ol) {
+                if (ol.li && ol.li.length > 100) {
+                    list = ol.li;
+                    return false;
+                }
+            });
+            _.forEach(list, function (li) {
+                if (_.has(li, 'a.content')) {
+                    vm.meritBadges[li.a.content] = {
+                        name: li.a.content,
+                        url: 'meritbadge.org' + _.get(li, 'a.href'),
+                        encodedName: _.replace(_.get(li, 'a.href'), '/wiki/index.php/', '')
+                    }
+                }
+            });
+            return $q.all(_.map(vm.meritBadges, function (badge) {
+                return $q.all({
+                    requirements: data.getWebpage(badge.url, 'table').then(function (table) {
+                        var listTable;
+                        table = _.castArray(table);
+                        _.forEach(table, function (table) {
+                            if (_.has(table, 'tbody.tr.td.a.name') && _.includes(_.get(table, 'tbody.tr.td.a.name'), 'merit_badge_requirements')) {
+                                listTable = table;
+                                return false;
+                            }
+                        });
+                        if (listTable) {
+                            vm.meritBadges[badge.name].requirements = listTable;
+                        }
+                    }),
+                    image: data.getWebpage(badge.url, 'img').then(function (img) {
+                        var mbImg;
+                        img = _.castArray(img);
+                        _.forEach(img, function (img) {
+                            if (img.src && _.includes(img.src, badge.encodedName)) {
+                                mbImg = img;
+                                return false;
+                            }
+                        });
+                        if (mbImg) {
+                            vm.meritBadges[badge.name].imgUrl = mbImg.src;
+                        }
+                    })
+                });
+            }));
         });
     }
 
--- app/scripts.js
+++ app/scripts.js
@@ -423,12 +423,60 @@ app.controller('requirements', ['dataService', '$q', function (data, $q) {
     'use strict';
     var vm = this;
     vm.data = data;
+    vm.meritBadges = {}
+    vm.meritBadgeUrl = 'meritbadge.org/wiki/index.php/Merit_Badges';
 
     vm.meritBadgeList = [];
 
     function getMeritBadges() {
-        return data.getWebpage('meritbadge.org/wiki/index.php/Merit_Badges', 'ol').then(function (webpage) {
-            console.log('got merit badges...hopefully');
+        return data.getWebpage(url, 'ol').then(function (ol) {
+            var list;
+            ol = _.castArray(ol);
+            _.forEach(ol, function (ol) {
+                if (ol.li && ol.li.length > 100) {
+                    list = ol.li;
+                    return false;
+                }
+            });
+            _.forEach(list, function (li) {
+                if (_.has(li, 'a.content')) {
+                    vm.meritBadges[li.a.content] = {
+                        name: li.a.content,
+                        url: 'meritbadge.org' + _.get(li, 'a.href'),
+                        encodedName: _.replace(_.get(li, 'a.href'), '/wiki/index.php/', '')
+                    }
+                }
+            });
+            return $q.all(_.map(vm.meritBadges, function (badge) {
+                return $q.all({
+                    requirements: data.getWebpage(badge.url, 'table').then(function (table) {
+                        var listTable;
+                        table = _.castArray(table);
+                        _.forEach(table, function (table) {
+                            if (_.has(table, 'tbody.tr.td.a.name') && _.includes(_.get(table, 'tbody.tr.td.a.name'), 'merit_badge_requirements')) {
+                                listTable = table;
+                                return false;
+                            }
+                        });
+                        if (listTable) {
+                            vm.meritBadges[badge.name].requirements = listTable;
+                        }
+                    }),
+                    image: data.getWebpage(badge.url, 'img').then(function (img) {
+                        var mbImg;
+                        img = _.castArray(img);
+                        _.forEach(img, function (img) {
+                            if (img.src && _.includes(img.src, badge.encodedName)) {
+                                mbImg = img;
+                                return false;
+                            }
+                        });
+                        if (mbImg) {
+                            vm.meritBadges[badge.name].imgUrl = mbImg.src;
+                        }
+                    })
+                });
+            }));
         });
     }
 
@@ -461,7 +509,7 @@ app.factory('dataService', ['$http', '$filter', '$q', function ($http, $filter, 
 
     data.getWebpage = function (url, classToGet) {
         return $http.get("https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20data.html.cssselect%20WHERE%20url%3D'" + encodeURI(url) + "'%20AND%20css%3D'" + classToGet + "'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", { withCredentials: false }).then(function (r) {
-            return r;
+            return r.data.query.results.results[classToGet];
         });
     }
 
